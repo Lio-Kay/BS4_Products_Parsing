a807dde857fdcc47bfa4d8aaea264e892ca254de
lio <121868806+Lio-Kay@users.noreply.github.com>
2023-12-19 14:01:43 +0700

    style: flake8 fixes

--- main.py
+++ main.py
@@ -4,24 +4,26 @@ import time
 import re
 import logging
 
-
 from bs4 import BeautifulSoup as BS
 from fake_useragent import UserAgent
 
 
 def main() -> None:
     """
-    Создаем логгер, хедер, получаем главную страницу через gather_main_page_data
-    на её основе парсим категории продуктов через gather_each_page_data
+    Создаем логер, хедер, получаем главную страницу через
+    gather_main_page_data на её основе парсим категории продуктов через
+    gather_each_page_data
     """
 
     # Настраиваем логгер
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s',
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)s:%(message)s',
                         filename='logs.txt', filemode='w', encoding='utf-8')
-    logging.debug(f'Запустили скрипт')
+    logging.debug('Запустили скрипт')
 
     # Сайт Мой здоровый рацион, страница таблиц калорийности
-    url = 'https://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie'
+    url = ('https://health-diet.ru/'
+           'table_calorie/?utm_source=leftMenu&utm_medium=table_calorie')
     # Создаем случайный user agent
     ua = UserAgent().firefox
     headers = {'User-Agent': ua}
@@ -34,10 +36,12 @@ def main() -> None:
 
     emd_main = time.perf_counter()
     logging.debug(f'Закончили собирать данные')
-    print(f'Закончили парсинг за {emd_main-start_main:.2f} сек, данные сохранены в папке data, логи в logs.txt')
+    print(f'Закончили парсинг за {emd_main-start_main:.2f} сек,'
+          f' данные сохранены в папке data, логи в logs.txt')
 
 
-def gather_main_page_data(headers: dict, url: str, retry: int = 3, sleep_time: int = 10) -> dict:
+def gather_main_page_data(headers: dict, url: str,
+                          retry: int = 3, sleep_time: int = 10) -> dict:
     """
     :param headers: user_agent из моего браузера
     :param url: url страницы таблиц калорийности из main
@@ -53,11 +57,14 @@ def gather_main_page_data(headers: dict, url: str, retry: int = 3, sleep_time: i
         logging.debug(f'Успешно получили данные с сайта через get запрос')
     except Exception as ex:
         if retry:
-            logging.error(f'Попытка:{retry}, статус: {requests.get(url=url, headers=headers).status_code}')
+            logging.error(f'Попытка:{retry}, статус: '
+                          f'{requests.get(url=url, headers=headers).status_code}')
             # Если все плохо - приостанавливаем код
             time.sleep(sleep_time)
-            # Возвращаем функцию, уменьшив кол-во попыток, и увеличив время ожидания
-            return gather_main_page_data(headers, url, retry=(retry - 1), sleep_time=(sleep_time+30))
+            # Возвращаем функцию, уменьшив кол-во попыток,
+            # и увеличив время ожидания
+            return (gather_main_page_data
+                    (headers, url, retry=(retry - 1), sleep_time=(sleep_time+30)))
         else:
             raise ex
 
@@ -76,18 +83,21 @@ def gather_main_page_data(headers: dict, url: str, retry: int = 3, sleep_time: i
         item_link = "https://health-diet.ru" + row.get('href')
         all_categories_dict.update({item_text: item_link})
 
-    # Создаем на его основе .json, просто для наглядности
-    with open(file='all_categories_dict.json', mode='w', encoding='utf-8') as file:
-        json.dump(obj=all_categories_dict, fp=file, indent=4, ensure_ascii=False)
+    # Создаем на его основе .json, для наглядности
+    with open(file='all_categories_dict.json', mode='w',
+              encoding='utf-8') as file:
+        json.dump(obj=all_categories_dict, fp=file,
+                  indent=4, ensure_ascii=False)
 
     end = time.perf_counter()
-    print(f'Собрали данные главной страницы и добавили ссылки на категории в файл'
-          f' all_categories_dict.json за {end-start:.2f} сек.')
+    print(f'Собрали данные главной страницы и добавили ссылки на'
+          f' категории в файл all_categories_dict.json за {end-start:.2f} сек.')
 
     return all_categories_dict
 
 
-def gather_each_page_data(counter: int, category: str, category_url: str, headers: dict) -> None:
+def gather_each_page_data(counter: int, category: str,
+                          category_url: str, headers: dict) -> None:
     """
     :param counter: Счетчик № категории
     :param category: Категория продукта
@@ -113,12 +123,14 @@ def gather_each_page_data(counter: int, category: str, category_url: str, header
         header_carbohydrates = product_headers[4].text
 
         # Записываем в csv
-        with open(file=f'data/{counter}.{category_fixed}.csv', mode='w', encoding='utf-8') as file:
+        with open(file=f'data/{counter}.{category_fixed}.csv', mode='w',
+                  encoding='utf-8') as file:
             file.write(f'{header_product},{header_energy_value},'
                        f'{header_proteins},{header_fats},{header_carbohydrates}\n')
 
         # Парсим данные по продуктам
-        product_table = soup.find(class_='mzr-tc-group-table').find('tbody').find_all('tr')
+        product_table = (soup.find(class_='mzr-tc-group-table')
+                         .find('tbody').find_all('tr'))
         for item in product_table:
             product_tds = item.find_all("td")
             title = product_tds[0].find("a").text
@@ -132,13 +144,17 @@ def gather_each_page_data(counter: int, category: str, category_url: str, header
             carbohydrates = carbohydrates.replace(',', '.')
 
             # Добавляем к csv в котором есть заголовки
-            with open(file=f'data/{counter}.{category_fixed}.csv', mode='a', encoding='utf-8') as file:
-                file.write(f'{title},{calories},{proteins},{fats},{carbohydrates}\n')
+            with open(file=f'data/{counter}.{category_fixed}.csv', mode='a',
+                      encoding='utf-8') as file:
+                file.write(f'{title},{calories},{proteins},'
+                           f'{fats},{carbohydrates}\n')
 
-        logging.debug(f'Закончили собирать данные из категории №{counter}: {category}')
+        logging.debug(f'Закончили собирать данные из категории №{counter}:'
+                      f' {category}')
     # Если отсутствует одна из страниц с данными, логируем и пропускаем её
     except AttributeError as exc:
-        logging.error(f'Ошибка AttributeError {exc} в категории №{counter}: {category}')
+        logging.error(f'Ошибка AttributeError {exc} в категории №{counter}:'
+                      f' {category}')
         pass
 
 
